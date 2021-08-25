6620b104c8cc00c0b683e2a926387da740b05375
rus_anonym <alexandrsemin2033@gmail.com>
2021-08-25 13:57:20 +0300

    fix

--- src/lib/commands/text/utils/mailing.ts
+++ src/lib/commands/text/utils/mailing.ts
@@ -1,16 +1,19 @@
 import TextCommand from "../../../utils/textCommand";
 
-new TextCommand(/^(?:профиль|проф)$/i, async (message) => {
-	const isEnable = message.state.args[1].toLowerCase() === "включить";
-	if (message.state.channel) {
-		message.state.channel.inform = isEnable;
-		return await message.reply(
-			`рассылка замен ${isEnable ? "включена" : "отключена"}.`,
-		);
-	} else {
-		message.state.user.inform = isEnable;
-		return await message.reply(
-			`рассылка замен ${isEnable ? "включена" : "отключена"}.`,
-		);
-	}
-});
+new TextCommand(
+	/^(?:изменения)(?:\s(включить|отключить))$/i,
+	async (message) => {
+		const isEnable = message.state.args[1].toLowerCase() === "включить";
+		if (message.state.channel) {
+			message.state.channel.inform = isEnable;
+			return await message.reply(
+				`рассылка замен ${isEnable ? "включена" : "отключена"}.`,
+			);
+		} else {
+			message.state.user.inform = isEnable;
+			return await message.reply(
+				`рассылка замен ${isEnable ? "включена" : "отключена"}.`,
+			);
+		}
+	},
+);
